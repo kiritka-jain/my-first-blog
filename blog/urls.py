bb63fdc83e115f0b5778b127eeda6388d2a8b0e0
Kirtika Jain <kirti@Saurabhs-MacBook-Air.local>
2022-06-30 22:04:44 +0530

    add sign in, sign up, and sign out functionality.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.home, name='home'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('sign_in', views.sign_in, name='sign_in'),
+    path('sign_out', views.sign_out, name='sign_out'),
+    path('post/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post, Comment
@@ -5,18 +8,75 @@ from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 
 
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def sign_up(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if User.objects.filter(username=username):
+            messages.error(request,"Username already exists.:Try some other username.")
+            return redirect('home')
+        if User.objects.filter(email=email):
+            messages.error(request,"email already registered.:Try some other email.")
+            return redirect('home')
+        if password != confirm_password:
+            messages.error(request, "Password mismatched!")
+            return redirect('home')
+        my_user = User.objects.create_user(username, email, password)
+        my_user.first_name = first_name
+        my_user.last_name = last_name
+        my_user.save()
+        messages.success(request, "Your account has been successfully created.")
+        return redirect('sign_in')
+    else:
+        return render(request, 'blog/sign_up.html')
+
+
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post_list')
+        else:
+            messages.error(request, "Bad Credentials.")
+            return redirect('home')
+    return render(request, 'blog/sign_in.html')
+
+
+def sign_out(request):
+    logout(request)
+    messages.success(request,"You have sucessfully logged out.")
+    return redirect('home')
+
+
 def post_list(request):
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
     post = get_object_or_404(Post, pk=pk)
     comments = Comment.objects.filter(post_id=pk)
     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
 
 
 def post_new(request):
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -31,6 +91,8 @@ def post_new(request):
 
 
 def post_edit(request, pk):
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -46,6 +108,8 @@ def post_edit(request, pk):
 
 
 def add_comment(request, pk):
+    if not request.user.is_authenticated:
+        return redirect('home')
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
@@ -59,7 +123,9 @@ def add_comment(request, pk):
     return render(request, 'blog/add_comment.html', {'form': form})
 
 
-def add_like(request,comment_id):
+def add_like(request, comment_id):
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
     comment = get_object_or_404(Comment, id=comment_id)
     if request.method == "POST":
         comment.like += 1
